5dcce2e88ab963ae2cd41de1cda480b22ef17a67
tridip9112 <debroytridip45@gmail.com>
2026-08-21 02:36:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,70 +1,54 @@
-# services/rule_engine.py
+# services/risk_aggregator.py
 
 
-class FraudRule:
-
-    def __init__(
-        self,
-        name,
-        condition,
-        weight
-    ):
-
-        self.name = name
-        self.condition = condition
-        self.weight = weight
-
-
-class RuleEngine:
+class RiskAggregator:
 
     def __init__(self):
 
-        self.rules = [
-
-            FraudRule(
-                "high_amount",
-                lambda x: x["amount"] > 50000,
-                20
-            ),
-
-            FraudRule(
-                "new_device",
-                lambda x: x["new_device"],
-                15
-            ),
-
-            FraudRule(
-                "new_location",
-                lambda x: x["new_location"],
-                10
-            ),
-
-            FraudRule(
-                "many_failures",
-                lambda x: x["failed_attempts"] >= 3,
-                15
-            ),
-
-            FraudRule(
-                "merchant_high_risk",
-                lambda x: x["merchant_risk"] >= 70,
-                25
-            )
-        ]
-
-    def evaluate(self, transaction):
-
-        triggered = []
-        score = 0
-
-        for rule in self.rules:
-
-            if rule.condition(transaction):
-
-                triggered.append(rule.name)
-                score += rule.weight
-
-        return {
-            "rule_score": min(score, 100),
-            "triggered_rules": triggered
+        self.weights = {
+            "ml": 0.40,
+            "rules": 0.20,
+            "merchant": 0.15,
+            "travel": 0.10,
+            "device": 0.15
         }
+
+    def calculate(
+        self,
+        ml_score,
+        rule_score,
+        merchant_score,
+        travel_score,
+        device_score
+    ):
+
+        final_score = (
+
+            ml_score *
+            self.weights["ml"]
+
+            +
+
+            rule_score *
+            self.weights["rules"]
+
+            +
+
+            merchant_score *
+            self.weights["merchant"]
+
+            +
+
+            travel_score *
+            self.weights["travel"]
+
+            +
+
+            device_score *
+            self.weights["device"]
+        )
+
+        return round(
+            min(final_score, 100),
+            2
+        )
