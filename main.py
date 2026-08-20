15980fe5142b54855d17b3d19a9af9df90c80141
tridip9112 <debroytridip45@gmail.com>
2026-08-21 02:06:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-# services/features.py
+# services/risk_engine.py
 
-def create_features(transaction, z_score):
-    return [
-        transaction.amount,
-        z_score,
-        transaction.failed_attempts,
-        int(transaction.new_device),
-        int(transaction.new_location),
-        transaction.transaction_hour,
-        transaction.previous_transaction_amount,
-        transaction.average_transaction_amount
-    ]
+def calculate_risk_score(
+    fraud_probability: float,
+    anomaly_score: float,
+    new_device: bool,
+    new_location: bool,
+    failed_attempts: int
+):
+
+    score = fraud_probability * 60
+
+    # Behavioral anomaly
+    score += min(abs(anomaly_score) * 5, 20)
+
+    # New device
+    if new_device:
+        score += 10
+
+    # New location
+    if new_location:
+        score += 5
+
+    # Failed authentication/payment attempts
+    score += min(failed_attempts * 2, 5)
+
+    return min(round(score), 100)
+
+
+def get_action(score: int):
+
+    if score <= 30:
+        return "ALLOW"
+
+    elif score <= 70:
+        return "ALERT"
+
+    return "BLOCK"
